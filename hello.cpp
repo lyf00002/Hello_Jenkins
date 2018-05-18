5c82eed4942e9b938eca0579afb1d9848862028b
leiyh <leiyh@melux.com>
2018-05-18 20:54:11 +0800

    change name

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void)
 {
-  cout << "Hello, lge again .\n";
+  cout << "Hello,  again .\n";
   cout << "Hello, Jenkins\n";
 
   cout << "I have successfully built and run\n";
